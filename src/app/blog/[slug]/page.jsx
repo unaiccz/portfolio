360dd661203986808b533b4423b2f1b01ae31369
unai <unaicc2003@gmail.com>
2024-03-10 14:23:15 +0100

    production_test

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,23 +1,24 @@
 import Image from "next/image";
 import styles from './page.module.css'
-import {getpost} from '@/utils/data'
+import {getpost, getuser} from '@/utils/data'
 
 async function page({params}) {
 const slug = params.slug
 const post =  await getpost(slug)
-  return (
+const user = await getuser(post.userid)
+return (
     <div className={styles.container}>
      <div className={styles.imgcontainer}>
-<Image src={post.img}  alt="post" fill className={styles.image}/>
+<Image src={post.img ? post.img : '/noavatar.jpg'}  alt="post" fill className={styles.image}/>
      </div>
      {/* text */}
      <div className={styles.textcontainer}>
 <h1 className={styles.title}>{post.title}</h1>
 <div className={styles.detail}>
-<Image src='/avatar.png' alt="avatar" height={50} width={50} className={styles.avatar}/>
+<Image src={user.img } alt="avatar" height={50} width={50} className={styles.avatar}/>
 <div className={styles.detailtext}>
 <span className={styles.dertailtitle}>Author </span>
-<span className={styles.detailtext}>Jerry Calderon</span>
+<span className={styles.detailtext}>{user.username}</span>
 </div>
 <div className={styles.detailtext}>
 <span className={styles.dertailtitle}>Published</span>
